7132fbdcbad3189872559066ccf3225917fa7414
Shanmukh raj <133531695+Shanmukh-star@users.noreply.github.com>
2023-06-12 11:01:42 +0530

    Simplified fractions.cpp

--- Simplified fractions.cpp
+++ Simplified fractions.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<string> simplifiedFractions(int n) {
-        set<string> v;
         vector<string> v1;
         string s;
         int i,j;
@@ -11,13 +10,12 @@ public:
            {
                for(i=j+1;i<=n;i++)
                {
-                  s=to_string(j/gcd(j,i))+"/"+to_string(i/gcd(j,i));
-                  v.insert(s);
+                   if(gcd(i,j)==1)
+                   {
+                       s=to_string(j)+"/"+to_string(i);
+                       v1.push_back(s);
+                   }
                }
-           }
-           for(auto i:v)
-           {
-               v1.push_back(i);
            }
         }
         return v1;
